841f768546f337a62a65654b75c96a5565e34b3d
xdrkush <drkushikush@gmail.com>
2020-04-14 19:47:21 +0200

    ajouter une image, suppression une image

--- api/controllers/articleController.js
+++ api/controllers/articleController.js
@@ -53,15 +53,19 @@ module.exports = {
   put: async(req, res, next) => {
     const dbArticle = await Article.findById(req.params.id),
       query = { _id: req.params.id },
-      noImg = req.files.length === 0
+      noImg = req.files.length === 0,
+      singleImg = req.files.length === 1,
+      multiple = req.files.length > 1
 
-    console.log('fzefezr')
+    console.log('Req.Body')
     console.log(req.body)
+    console.log('Req.Files')
     console.log(req.files)
-    console.log('fonction length')
-    console.log(noImg)
 
     if (req.body.title && noImg) {
+      /*
+       *  Changer Texte
+       **********************/
       console.log('edit title (no file)')
       Article.updateOne(query, {
           title: req.body.title
@@ -70,7 +74,115 @@ module.exports = {
           if (err) console.log(err)
           else res.redirect('/article')
         })
-    } else if (req.files) {
+
+    } else if (req.body.addImg === '') {
+      /*
+       *  Ajouter Une Image
+       **********************/
+      const dbArticle = await Article.findById(req.params.id),
+        query = {_id: req.params.id},
+        dbFiles = dbArticle.galleryImg,
+        files = req.files,
+        arrayFiles = []
+
+      console.log('?? AddImage')
+      console.log(dbFiles)
+
+      // Boucle pour chercher les files existant dans la DB et les ajouter au tableau arrayFiles
+      for (let i = 0; i < dbFiles.length; i++) {
+        const dbFilename = dbFiles[i].filename
+        if (dbFiles) {
+          console.log(dbFiles[i].filename)
+          arrayFiles.push({
+            name: dbFiles[i].name,
+            filename: dbFiles[i].filename,
+            orifginalname: dbFiles[i].name
+          })
+        }
+      }
+
+      // Boucle pour chercher les req.files et les ajouter au tableau arrayFiles
+      for (let i = 0; i < files.length; i++) {
+        const dbFilename = files[i].filename
+        if (files) {
+          console.log(files[i].filename)
+          arrayFiles.push({
+            name: files[i].filename,
+            filename: `/assets/images/${files[i].filename}`,
+            orifginalname: files[i].originalname
+          })
+        }
+      }
+
+      console.log('?? Array files')
+      console.log(arrayFiles)
+
+      // Fonction update Mongoose
+      Article.updateOne(query, {
+        ...req.body,
+        galleryImg: arrayFiles
+      },
+      (err) => {
+        if (!err) {
+          res.redirect('/article')
+        } else {
+          return res.send(err)
+        }
+      })
+
+    } else if (req.body.deleteImg) {
+      /*
+       *  Supprimer Une Image
+       **********************/
+      console.log('delete single img')
+      const dbArticle = await Article.findById(req.params.id),
+        files = dbArticle.galleryImg
+      arrayFiles = []
+
+      console.log('?? req.body')
+      console.log(req.body)
+      console.log('?? dbArticle')
+      console.log(dbArticle)
+
+      for (let i = 0; i < files.length; i++) {
+        const dbFilename = files[i].name
+        if (dbFilename !== req.body.deleteImg) {
+          console.log(dbFilename)
+          arrayFiles.push({
+            name: files[i].name,
+            filename: files[i].filename,
+            originalname: files[i].name
+          })
+        }
+      }
+
+      console.log('?? arrayfiles')
+      console.log(arrayFiles)
+
+      Article.updateOne(query, {
+          ...req.body,
+          galleryImg: arrayFiles
+        },
+        (err) => {
+          if (!err) {
+            fs.unlink(path.resolve('public/images/' + req.body.deleteImg),
+              (err) => {
+                if (err) throw err
+              })
+            res.redirect('/article')
+          } else {
+            return res.send(err)
+          }
+        })
+
+    } else if (singleImg) {
+
+      console.log('Single')
+
+    } else if (multiple) {
+      /*
+       *  Éditer toutes les images
+       ****************************/
       const dbArticle = await Article.findById(req.params.id),
         files = req.files,
         existImg = dbArticle.galleryImg,
@@ -98,7 +210,7 @@ module.exports = {
       console.log(existImg)
 
       Article.updateOne(query, {
-        ...req.body,
+          ...req.body,
           galleryImg: arrayFiles
         },
         (err) => {
